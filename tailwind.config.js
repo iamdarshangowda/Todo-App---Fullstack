149e06b8f19703698d955bb5e36f1c0a28130cf6
Darshan Gowda <iamdarshangowda@gmail.com>
2023-04-18 21:25:51 +0530

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,54 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-noto)", ...fontFamily.sans],
+      },
+      colors: {
+        grey: {
+          DEFAULT: "#333333",
+          0: "#FFFFFF",
+          20: "#A0A0A0",
+          30: "#707070",
+          40: "#4B4B4B",
+          50: "#3C3C3C",
+          60: "#2F2F2F",
+          70: "#242424",
+          80: "#1E1E1E",
+          85: "#22252A",
+          90: "#121212",
+        },
+      },
+      fontSize: {
+        "heading-1/h2": ["2.5rem", { fontWeight: "700" }],
+        "heading-3/h1": ["20px", { fontWeight: "500" }],
+        "heading-3/h2": ["20px", { fontWeight: "700" }],
+        "button/b1": ["12px", { fontWeight: "600" }],
+        "body-1/b1": ["16px", { fontWeight: "400" }],
+        "body-1/b2": ["16px", { fontWeight: "500" }],
+        "body-1/b3": ["16px", { fontWeight: "600" }],
+        "body-2/b1": ["14px", { fontWeight: "400" }],
+        "body-2/b2": ["14px", { fontWeight: "500" }],
+        "body-2/b3": ["14px", { fontWeight: "600" }],
+        "body-3/b1": ["12px", { fontWeight: "400" }],
+        "body-3/b2": ["12px", { fontWeight: "500" }],
+        "body-3/b3": ["12px", { fontWeight: "600" }],
+        "caption/c1": ["10px", { fontWeight: "400" }],
+        "caption/c2": ["10px", { fontWeight: "700" }],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
