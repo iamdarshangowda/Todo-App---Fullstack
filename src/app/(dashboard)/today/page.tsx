1ae2c15f5b9244e6a28455bf9598154b67ece5cf
Darshan Gowda <iamdarshangowda@gmail.com>
2023-06-22 23:29:08 +0530

    tasks layout optimized

--- src/app/(dashboard)/today/page.tsx
+++ src/app/(dashboard)/today/page.tsx
@@ -1,47 +1,19 @@
 'use client';
 
-import AddTaskModal from '@components/add-tasks/addTasksModal';
-import SecondaryButton from '@components/common/buttons/secondaryButton';
-import { AddIcon, RecMicIcon } from '@components/common/icons/icons';
 import SingleTask from '@components/common/ui-components/singleTask';
-import TaskHeaderwithCount from '@components/common/ui-components/taskHeaderwithCount';
 import TaskPageLayout from '@components/ui-layout/taskPageLayout';
 import { useUIHelperContext } from '@context/useUIHelperContext';
-import React, { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import React, { ChangeEvent, useMemo, useState } from 'react';
 import { get } from '../../../config/axiosClient';
-import SingleTaskSkeleton from '@components/common/skeletons/singleTaskSkeleton';
-import ViewTaskModal from '@components/view-tasks/viewTaskModal';
 import { ISingleTask } from '@utils/types';
-import { useDataStoreContext } from '@context/useDataStoreContext';
-import { DELAY, initialTask } from '@utils/initialData';
-import { useToggleContext } from '@context/useToggleContext';
-import isMobileDevice from '@utils/detectUserDevice';
+import { DELAY } from '@utils/initialData';
 import { debounce } from '@utils/debounce';
-import SearchBar from '@components/menu/searchBar';
 
 const Today = () => {
-  const [showAddTasks, setShowAddTasks] = useState<boolean>(false);
   const [viewTasks, setViewTasks] = useState<boolean>(false);
-  const { setBlurBackground, loading, setLoading } = useUIHelperContext();
-  const { setSingleTaskData } = useDataStoreContext();
+  const { loading, setLoading } = useUIHelperContext();
   const [tasks, setTasks] = useState([]);
-  const { setHideMenu } = useToggleContext();
   const [searchText, setSearchText] = useState<string>('');
-
-  useEffect(() => {
-    if (showAddTasks || viewTasks) {
-      setBlurBackground(true);
-    } else {
-      setBlurBackground(false);
-    }
-
-    if (!showAddTasks && !viewTasks) setSingleTaskData(initialTask);
-  }, [showAddTasks, viewTasks]);
-
-  const handleAddTask = () => {
-    setSingleTaskData(initialTask);
-    setShowAddTasks((prev) => !prev);
-  };
 
   const handleSearchChange = debounce((event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -72,64 +44,27 @@ const Today = () => {
     }
   };
 
-  useEffect(() => {
-    handleGetAllTasks();
-    if (isMobileDevice()) {
-      setHideMenu(true);
-    }
-  }, []);
-
   return (
-    <TaskPageLayout>
-      <TaskHeaderwithCount title={'Today'} count={tasks.length} loading={loading} />
-
-      <div className="flex gap-2 sm:gap-4 w-full justify-between sm:justify-start">
-        <SecondaryButton text="Add Task" onClick={handleAddTask} icon={<AddIcon />} />
-        <SecondaryButton
-          text="Rec Audio"
-          onClick={() => setShowAddTasks((prev) => !prev)}
-          icon={<RecMicIcon />}
-        />
-      </div>
-
-      <AddTaskModal
-        showAddTasks={showAddTasks}
-        setShowAddTasks={setShowAddTasks}
-        callback={handleGetAllTasks}
-      />
-
-      <ViewTaskModal
-        viewTasks={viewTasks}
-        setViewTasks={setViewTasks}
-        setShowAddTasks={setShowAddTasks}
-        callback={handleGetAllTasks}
-      />
-
-      {isMobileDevice() && <SearchBar onChange={handleSearchChange} />}
-
-      <div className="flex flex-col space-y-2 overflow-y-scroll h-[calc(95vh-200px)] last:pb-5 scrollbar-hide">
-        {loading ? (
-          Array(4)
-            .fill('')
-            .map((data, index) => <SingleTaskSkeleton key={index} />)
+    <TaskPageLayout
+      header="Today"
+      count={tasks.length}
+      loading={loading}
+      handleGetAllTasks={handleGetAllTasks}
+      viewTasks={viewTasks}
+      setViewTasks={setViewTasks}
+      handleSearchChange={handleSearchChange}
+    >
+      <>
+        {filteredTasks.length ? (
+          filteredTasks.map((task: ISingleTask) => (
+            <SingleTask taskData={task} key={task.title} setViewTasks={setViewTasks} />
+          ))
         ) : (
-          <>
-            {filteredTasks.length ? (
-              filteredTasks.map((task: ISingleTask) => (
-                <SingleTask
-                  taskData={task}
-                  key={task.title}
-                  setViewTasks={setViewTasks}
-                />
-              ))
-            ) : (
-              <h2 className="text-grey-40 text-body-1/b2 text-center mt-5">
-                No Tasks Added!
-              </h2>
-            )}
-          </>
+          <h2 className="text-grey-40 text-body-1/b2 text-center mt-5">
+            No Tasks Added for Today!
+          </h2>
         )}
-      </div>
+      </>
     </TaskPageLayout>
   );
 };
--- src/app/(dashboard)/upcoming/page.tsx
+++ src/app/(dashboard)/upcoming/page.tsx
@@ -1,47 +1,19 @@
 'use client';
 
-import AddTaskModal from '@components/add-tasks/addTasksModal';
-import SecondaryButton from '@components/common/buttons/secondaryButton';
-import { AddIcon, RecMicIcon } from '@components/common/icons/icons';
 import SingleTask from '@components/common/ui-components/singleTask';
-import TaskHeaderwithCount from '@components/common/ui-components/taskHeaderwithCount';
 import TaskPageLayout from '@components/ui-layout/taskPageLayout';
 import { useUIHelperContext } from '@context/useUIHelperContext';
-import React, { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import React, { ChangeEvent, useMemo, useState } from 'react';
 import { get } from '../../../config/axiosClient';
-import SingleTaskSkeleton from '@components/common/skeletons/singleTaskSkeleton';
-import ViewTaskModal from '@components/view-tasks/viewTaskModal';
 import { ISingleTask } from '@utils/types';
-import { useDataStoreContext } from '@context/useDataStoreContext';
-import { DELAY, initialTask } from '@utils/initialData';
-import isMobileDevice from '@utils/detectUserDevice';
-import { useToggleContext } from '@context/useToggleContext';
-import SearchBar from '@components/menu/searchBar';
+import { DELAY } from '@utils/initialData';
 import { debounce } from '@utils/debounce';
 
 const Upcoming = () => {
-  const [showAddTasks, setShowAddTasks] = useState<boolean>(false);
   const [viewTasks, setViewTasks] = useState<boolean>(false);
-  const { setBlurBackground, loading, setLoading } = useUIHelperContext();
-  const { setSingleTaskData } = useDataStoreContext();
-  const { setHideMenu } = useToggleContext();
+  const { loading, setLoading } = useUIHelperContext();
   const [tasks, setTasks] = useState([]);
   const [searchText, setSearchText] = useState<string>('');
-
-  useEffect(() => {
-    if (showAddTasks || viewTasks) {
-      setBlurBackground(true);
-    } else {
-      setBlurBackground(false);
-    }
-
-    if (!showAddTasks && !viewTasks) setSingleTaskData(initialTask);
-  }, [showAddTasks, viewTasks]);
-
-  const handleAddTask = () => {
-    setSingleTaskData(initialTask);
-    setShowAddTasks((prev) => !prev);
-  };
 
   const handleSearchChange = debounce((event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -73,64 +45,27 @@ const Upcoming = () => {
     }
   };
 
-  useEffect(() => {
-    handleGetAllTasks();
-    if (isMobileDevice()) {
-      setHideMenu(true);
-    }
-  }, []);
-
   return (
-    <TaskPageLayout>
-      <TaskHeaderwithCount title={'Upcoming'} count={tasks.length} loading={loading} />
-
-      <div className="flex gap-2 sm:gap-4 w-full justify-between sm:justify-start">
-        <SecondaryButton text="Add Task" onClick={handleAddTask} icon={<AddIcon />} />
-        <SecondaryButton
-          text="Rec Audio"
-          onClick={() => setShowAddTasks((prev) => !prev)}
-          icon={<RecMicIcon />}
-        />
-      </div>
-
-      <AddTaskModal
-        showAddTasks={showAddTasks}
-        setShowAddTasks={setShowAddTasks}
-        callback={handleGetAllTasks}
-      />
-
-      <ViewTaskModal
-        viewTasks={viewTasks}
-        setViewTasks={setViewTasks}
-        setShowAddTasks={setShowAddTasks}
-        callback={handleGetAllTasks}
-      />
-
-      {isMobileDevice() && <SearchBar onChange={handleSearchChange} />}
-
-      <div className="flex flex-col space-y-2 overflow-y-scroll h-[calc(95vh-200px)] last:pb-5 scrollbar-hide">
-        {loading ? (
-          Array(4)
-            .fill('')
-            .map((data, index) => <SingleTaskSkeleton key={index} />)
+    <TaskPageLayout
+      header="Upcoming"
+      count={tasks.length}
+      loading={loading}
+      handleGetAllTasks={handleGetAllTasks}
+      viewTasks={viewTasks}
+      setViewTasks={setViewTasks}
+      handleSearchChange={handleSearchChange}
+    >
+      <>
+        {filteredTasks.length ? (
+          filteredTasks.map((task: ISingleTask) => (
+            <SingleTask taskData={task} key={task.title} setViewTasks={setViewTasks} />
+          ))
         ) : (
-          <>
-            {filteredTasks.length ? (
-              filteredTasks.map((task: ISingleTask) => (
-                <SingleTask
-                  taskData={task}
-                  key={task.title}
-                  setViewTasks={setViewTasks}
-                />
-              ))
-            ) : (
-              <h2 className="text-grey-40 text-body-1/b2 text-center mt-5">
-                No Tasks Added!
-              </h2>
-            )}
-          </>
+          <h2 className="text-grey-40 text-body-1/b2 text-center mt-5">
+            No Upcoming Tasks!
+          </h2>
         )}
-      </div>
+      </>
     </TaskPageLayout>
   );
 };
