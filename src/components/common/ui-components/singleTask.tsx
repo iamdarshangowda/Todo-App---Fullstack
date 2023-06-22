92a63efa7f34bf36f2fff98a12be187b02004c4d
Darshan Gowda <iamdarshangowda@gmail.com>
2023-06-23 00:42:26 +0530

    single task UI update

--- src/components/common/ui-components/singleTask.tsx
+++ src/components/common/ui-components/singleTask.tsx
@@ -30,40 +30,39 @@ const SingleTask = (props: ISingleTaskProps) => {
     <div
       onClick={handleViewTask}
       className="hover:bg-grey-10 m-0 sm:mx-2 sm:first:mt-2 p-2 rounded-lg hover:cursor-pointer
-    hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+     sm:hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
     >
-      <div className="flex gap-4 p-1">
-        {/* <div className="w-5 h-5">
-          <input type="checkbox" id={title} name="singletask" value="task1" size={4} />
-        </div> */}
-        <h1 className="text-grey-40 flex-grow hover:cursor-pointer text-body-1/b2">
-          {capitalizeFirstLetter(title)}
-        </h1>
-        <div className="pr-1 sm:pr-5">
+      <div className="flex gap-4 p-1 items-center">
+        <div>
+          <h1 className="text-grey-40 flex-grow hover:cursor-pointer text-body-1/b2">
+            {capitalizeFirstLetter(title)}
+          </h1>
+
+          <div className="flex gap-8 items-center py-3">
+            {due_date && (
+              <div className="flex gap-2 items-center">
+                <CalendarMenuIcon />
+                <h3 className="text-grey-60 text-body-2/b1">
+                  {new Date(due_date ?? '').toLocaleString()}
+                </h3>
+              </div>
+            )}
+            {list_type && (
+              <div className="flex gap-2 items-center">
+                <ListIocnBox
+                  size={'!h-4 w-4 rounded-sm'}
+                  bgColor={list_type === 'work' ? '!bg-yellow' : '!bg-red-500'}
+                />
+                <h3 className="text-grey-60 text-body-2/b1">
+                  {capitalizeFirstLetter(list_type)}
+                </h3>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="pr-1 sm:pr-5 ml-auto">
           <RightIcon />
         </div>
-      </div>
-
-      <div className="flex gap-8 items-center p-1">
-        {due_date && (
-          <div className="flex gap-2 items-center">
-            <CalendarMenuIcon />
-            <h3 className="text-grey-60 text-body-2/b1">
-              {new Date(due_date ?? '').toLocaleString()}
-            </h3>
-          </div>
-        )}
-        {list_type && (
-          <div className="flex gap-2 items-center">
-            <ListIocnBox
-              size={'!h-4 w-4 rounded-sm'}
-              bgColor={list_type === 'work' ? '!bg-yellow' : '!bg-red-500'}
-            />
-            <h3 className="text-grey-60 text-body-2/b1">
-              {capitalizeFirstLetter(list_type)}
-            </h3>
-          </div>
-        )}
       </div>
     </div>
   );
