83b52bdf69e01c1412e999655ca86c0c92a04bfd
Darshan Gowda <iamdarshangowda@gmail.com>
2023-07-18 22:07:58 +0530

    auth guard added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,11 +5,39 @@ import SuccessToast from '@components/common/toasts/successToast';
 import MenuSidebar from '@components/menu/menuSidebar';
 import { useToggleContext } from '@context/useToggleContext';
 import { useUIHelperContext } from '@context/useUIHelperContext';
-import React from 'react';
+import { useUserDataContext } from '@context/useUserContext';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
+import { get } from '../../config/axiosClient';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
   const { blurBackground } = useUIHelperContext();
   const { showSuccessToast, showErrorToast } = useToggleContext();
+  const { userAuthData, setUserAuthData } = useUserDataContext();
+
+  const getUserProfile = async () => {
+    await get('user/profile').then(
+      (data) => {
+        if (data.data.user) {
+          setUserAuthData(data.data.user);
+        } else {
+          router.push('/');
+        }
+      },
+      (err: any) => {
+        router.push('/');
+        console.log(err.message);
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (!userAuthData._id) {
+      getUserProfile();
+    }
+  }, []);
+
   return (
     <div
       className={`fixed top-0 bottom-0 gap-2 flex p-4 w-full ${
