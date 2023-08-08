ddc2c65edba2fe5b866d3553cf04c6e5cea100cb
Darshan Gowda <iamdarshangowda@gmail.com>
2023-08-08 12:59:10 +0530

    comment proxy

--- src/utils/setupProxy.js
+++ src/utils/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: process.env.TODO_BACKED_PORT,
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'https://localhost:5000',
+//       changeOrigin: true,
+//     })
+//   );
+// };
