eee483942b867fc472a02da1159af21f0a018e8d
Darshan Gowda <iamdarshangowda@gmail.com>
2023-08-18 20:13:06 +0530

    add sticky button

--- src/components/sticky/addSticky.tsx
+++ src/components/sticky/addSticky.tsx
@@ -20,7 +20,6 @@ interface IAddStickyProps {
 const AddSticky = (props: IAddStickyProps) => {
   const { callback } = props;
 
-  const [isAdding, setIsAdding] = useState(false);
   const { loading, setLoading } = useUIHelperContext();
   const { setShowSuccessToast, setShowErrorToast } = useToggleContext();
   const { setTasksCount } = useDataStoreContext();
@@ -37,11 +36,6 @@ const AddSticky = (props: IAddStickyProps) => {
     setStickyData((prev) => ({ ...prev, text: value }));
   };
 
-  const handleCloseInput = () => {
-    setIsAdding(false);
-    setStickyData(initialData);
-  };
-
   const handleSaveSticky = async () => {
     if (!stickyData.text) {
       setShowErrorToast({ show: true, message: 'Text is empty!' });
@@ -52,7 +46,6 @@ const AddSticky = (props: IAddStickyProps) => {
       setLoading(true);
       stickyData.stickyColor = generateRandomColor();
       await post('sticky', stickyData).then((data) => {
-        setIsAdding(false);
         callback();
         getAllCount(setTasksCount);
         setStickyData(initialData);
@@ -71,33 +64,23 @@ const AddSticky = (props: IAddStickyProps) => {
       className="relative rounded-xl shadow-sm shadow-grey-50 dark:shadow-grey-40 w-full h-full flex justify-center items-center
      text-grey-80 bg-cream min-h-[150px] md:min-h-[200px]"
     >
-      {isAdding ? (
-        <div className="w-full h-full">
-          <textarea
-            className={`rounded-xl w-full h-full border-none outline-none pt-8 md:pt-10 px-2 md:px-5 placeholder:text-grey-20
+      <div className="w-full h-full">
+        <textarea
+          className={`rounded-xl w-full h-full border-none outline-none pt-8 md:pt-10 px-2 md:px-5 placeholder:text-grey-20
             placeholder:text-body-2/b2 disabled:text-grey-20 ${bg}`}
-            onChange={handleStickyChange}
-            value={stickyData.text}
-            autoComplete="off"
-            placeholder={'Type here...'}
-            disabled={loading}
-          />
-          <div className="absolute top-2 right-2 md:top-4 md:right-4 cursor-pointer">
-            <div onClick={handleCloseInput}>
-              <CloseIcon />
-            </div>
-          </div>
-          <div className="absolute  bottom-2 right-2 md:bottom-4 md:right-4 cursor-pointer bg-yellow rounded-xl flex items-center text-caption/c2 md:text-body-2/b2 p-0 md:p-1">
-            <div onClick={handleSaveSticky}>
-              <TextButton text="Save" />
-            </div>
+          onChange={handleStickyChange}
+          value={stickyData.text}
+          autoComplete="off"
+          placeholder={'Type here...'}
+          disabled={loading}
+        />
+
+        <div className="absolute  bottom-2 right-2 md:bottom-4 md:right-4 cursor-pointer bg-yellow rounded-xl flex items-center text-caption/c2 md:text-body-2/b2 p-0 md:p-1">
+          <div onClick={handleSaveSticky}>
+            <TextButton text="Save" />
           </div>
         </div>
-      ) : (
-        <div className="cursor-pointer" onClick={() => setIsAdding(true)}>
-          <AddIcon size={'w-8 h-8'} />
-        </div>
-      )}
+      </div>
     </div>
   );
 };
